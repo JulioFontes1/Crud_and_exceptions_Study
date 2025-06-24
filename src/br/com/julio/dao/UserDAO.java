d743c20e0ea68e01ac6659c20a4acac7dc924d3e
JulioFontes1 <julio2006ofc@gmail.com>
2025-06-24 18:27:12 -0300

    Primeiro commit preparando o CRUD

---
+++ src/br/com/julio/dao/UserDAO.java
@@ -0,0 +1,49 @@
+package br.com.julio.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.julio.exception.UserNotFoundException;
+import br.com.julio.model.UserModel;
+
+public class UserDAO {
+
+    private long nextId = 1L;
+
+    private final List<UserModel> models = new ArrayList<>();
+
+    public UserModel save(final UserModel model){
+        model.setId(nextId++);
+        models.add(model);
+        return model;
+    }
+
+    public UserModel update(final UserModel model){
+        var toUpdate = findById(model.getId());
+        models.remove(toUpdate);
+        models.add(model);
+        return model;
+    }
+
+    public void delete(final long id){
+
+        var toDelete = findById(id);
+        models.remove(toDelete);
+
+    }
+
+    public UserModel findById(final long id){
+
+        var message = String.format("Usuário com id %s não encontrado!", id);
+
+        return models.stream()
+                .filter(u -> u.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new UserNotFoundException(message));
+
+    }
+
+    public List<UserModel> findAll (){
+        return models;
+    }
+}
